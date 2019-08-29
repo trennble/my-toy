f2c98457383f83e86aa081146f8342b3687e6e3f
jiangbo <trennble@gmail.com>
2019-08-29 14:02:30 +0800

    BeanPropertyRowMapper

---
+++ src/main/java/com/trennble/rowmapper/BeanPropertyRowMapperDemo.java
@@ -0,0 +1,11 @@
+package com.trennble.rowmapper;
+
+/**
+ * @ClasssName BeanPropertyRowMapperDemo
+ * @Description 记录使用BeanPropertyRowMapper来对template的结果进行转换
+ * @Author sycamore
+ * @Date 2019-8-29 14:01
+ * @Version 1.0
+ **/
+public class BeanPropertyRowMapperDemo {
+}
