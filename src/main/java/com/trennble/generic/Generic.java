e1eb61d371e9fb68e33b3f2179e62b377661d8ea
jiangbo <trennble@gmail.com>
2019-09-06 16:34:31 +0800

    泛型

---
+++ src/main/java/com/trennble/generic/Generic.java
@@ -0,0 +1,38 @@
+package com.trennble.generic;
+
+import java.util.Collection;
+
+/**
+ * @ClasssName Generic
+ * @Description TODO
+ * @Author sycamore
+ * @Date 2019-9-6 16:14
+ * @Version 1.0
+ **/
+public class Generic {
+
+    public void test(){
+
+    }
+
+    public void testSub(Collection<? extends Base> para){
+        // 编译不通过
+        para.add(new Sub());
+        para.add(new Base());
+    }
+
+    public <T> void test(Collection<T> collection){
+        collection.add((T)new Integer(12));
+        collection.add((T)"123");
+    }
+
+    public void testSuper(Collection<? super Sub> para){
+        para.add(new Sub());// 编译通过
+        para.add(new Base());// 编译不通过
+    }
+
+
+    private static class Base{ }
+
+    private static class Sub extends Base{ }
+}
