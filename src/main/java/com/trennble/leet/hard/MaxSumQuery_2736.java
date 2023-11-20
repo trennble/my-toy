c775b45512c868c13dae5d998dc411c94ac6609b
jiangbo <jiangbo@hongsong.club>
2023-11-20 16:31:49 +0800

    feat: maxSumQuery2736

---
+++ src/main/java/com/trennble/leet/hard/MaxSumQuery_2736.java
@@ -0,0 +1,13 @@
+package com.trennble.leet.hard;
+
+import java.util.*;
+
+public class MaxSumQuery_2736 {
+
+
+    public static void main(String[] args) {
+        MaxSumQuery_2736 maxSumQuery2736 = new MaxSumQuery_2736();
+        // maxSumQuery2736.maximumSumQueries(new int[]{4,3,1,2},new int[]{2,4,9,5},new int[][]{{4,1},{1,3},{2,5}});
+    }
+
+}
